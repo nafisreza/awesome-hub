79a499b167ed9b54135a57071de493ff63fe6f5a
satya <satyanshofficial@gmail.com>
2025-10-06 16:39:56 +0530

    Switched from next/lint to ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,17 +9,9 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default [
   {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
+    ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"],
   },
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
-
-export default eslintConfig;
